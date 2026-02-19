6b7b4f618d488dc75a6a9bec3dae9c69522abb41
Serhii-Halenko <galenkosergij830@gmail.com>
2026-02-19 18:39:59 +0200

    Refactor: Remove CatalogButton, Header, and SearchInput components and their styles; add new implementations

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -49,7 +49,9 @@ export default function Header() {
         </div>
       </div>
 
-      <SearchInput />
+      <div className={styles.searchWrap}>
+        <SearchInput />
+      </div>
     </header>
   );
 }
